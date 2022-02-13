aa2fdf5a7a9e1c12fb98267add52cec8040be0a0
navateja <navateja@navatejas-MBP.lan>
2022-02-13 00:33:40 -0600

    solution pushed

--- fuzzy_phrases/solution.py
+++ fuzzy_phrases/solution.py
@@ -1,8 +1,25 @@
 import json
-
+import re
 def phrasel_search(P, Queries):
     # Write your solution here
-    ans = [[]]
+    ans = []
+    # Iterating through each query
+    for q in Queries:
+        temp_ans = []
+        # Iterating through each phrase
+        for phrase in P:
+            words = phrase.split(" ")
+            start = words[0]
+            end = words[-1]
+            # Using regex to fing the substring
+            regex = r'@\ (.*?)$'.replace("@", start).replace("$", end)
+            substrings = re.findall(regex, q)
+            for substr in substrings:
+                substr = start + " " + substr + end
+                substr_split = substr.split(" ")
+                if (len(substr_split) == len(words)) or (len(substr_split) == len(words)+1):
+                    temp_ans.append(substr)
+        ans.append(temp_ans)
     return ans
 
 if __name__ == "__main__":
@@ -10,4 +27,5 @@ if __name__ == "__main__":
         sample_data = json.loads(f.read())
         P, Queries = sample_data['phrases'], sample_data['queries']
         returned_ans = phrasel_search(P, Queries)
+        print(returned_ans)
         print('============= ALL TEST PASSED SUCCESSFULLY ===============')
